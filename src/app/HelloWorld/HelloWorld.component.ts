b5341b18f7806ecfb1695a8a22787e310470ec09
Bytor25 <juan.hincapie0078@uco.net.co>
2024-05-02 10:51:35 -0500

    Primer commit front

---
+++ src/app/HelloWorld/HelloWorld.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-HelloWorld',
+    templateUrl: './HelloWorld.component.html',
+    styleUrls: ['./HelloWorld.component.css'] 
+})
+
+export class HelloWorldComponent{
+    
+}
